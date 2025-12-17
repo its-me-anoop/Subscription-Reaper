e276d5a21cffb4a7cdf537a469e2a0736e4cdc12
Anoop Jose <anoop@flutterly.co.uk>
2025-12-17 20:59:55 +0000

    Initial Commit

---
+++ Subscription Reaper/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Subscription Reaper
+//
+//  Created by Anoop Jose on 17/12/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Subscription ReaperTests/Subscription_ReaperTests.swift
@@ -0,0 +1,17 @@
+//
+//  Subscription_ReaperTests.swift
+//  Subscription ReaperTests
+//
+//  Created by Anoop Jose on 17/12/2025.
+//
+
+import Testing
+@testable import Subscription_Reaper
+
+struct Subscription_ReaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
