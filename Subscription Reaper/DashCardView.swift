8b32c4d4800074dcd18f74e064c2d3ff77dc56b3
Anoop Jose <anoop@flutterly.co.uk>
2025-12-17 22:06:08 +0000

    feat: Redesign dashboard UI with animated background, new dashboard card, and enhanced subscription list presentation.

---
+++ Subscription Reaper/DashCardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashCardView.swift
+//  Subscription Reaper
+//
+//  Created by Anoop Jose on 17/12/2025.
+//
+
+import SwiftUI
+
+struct DashCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashCardView()
+}
